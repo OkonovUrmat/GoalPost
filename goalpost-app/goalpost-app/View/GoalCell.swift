e5f94c26aaf705de28b8057b89f8d0ed826adbc3
OkonovUrmat <okonov.okonov@gmail.com>
2020-05-22 17:50:39 +0600

    Add and configure Goal Cell

---
+++ goalpost-app/goalpost-app/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Urmat on 5/22/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+}
